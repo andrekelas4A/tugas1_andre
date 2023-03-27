c894de1d1241549f315bcd1ee475cc24ef8ddfd0
andrekelas4A <yuliushermawan813@gmail.com>
2023-03-27 15:58:10 -0700

    web-yuliusandredh

---
+++ OOP/constructor.php
@@ -0,0 +1,28 @@
+<?php 
+
+class Produk {
+    public $judul, $penulis, $penerbit, $harga;
+
+    public function _construct(){
+        echo "hello";
+    }
+    public function getLabel(){
+        return "$this->judul, $this->penulis";
+    }
+}
+
+$produk1 = new Produk();
+$produk1 ->judul = "Naruto";
+$produk1 ->penulis = "Andre";
+
+$produk2 = new Produk();
+$produk2 ->judul = "Kimi no nawa";
+$produk2 ->penulis = "Dwi";
+
+//var_dump($produk1);
+//var_dump($produk2);
+
+echo $produk1 -> getLabel();
+echo "<br>";
+echo $produk2 -> getLabel();
+?>
