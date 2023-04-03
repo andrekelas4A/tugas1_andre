52e6746127218688cf6d4c759abcdc41b53d7cce
andrekelas4A <yuliushermawan813@gmail.com>
2023-04-03 14:05:02 -0700

    tambah file object_type

---
+++ OOP/object_type.php
@@ -0,0 +1,48 @@
+<?php 
+
+class Produk {
+    public $judul, $penulis, $penerbit, $harga;
+
+    public function __construct($judul='judul', $penulis='penulis', $penerbit='penerbit', $harga=0){
+        //echo "hello";
+        $this ->judul = $judul;
+        $this ->penulis = $penulis;
+        $this ->penerbit = $penerbit;
+        $this ->harga = $harga;
+
+    }
+    public function getLabel(){
+        return "$this->judul, $this->penulis";
+    }
+}
+
+class CetakInfoProduk{
+    public function cetak(Produk $produk){
+        $str = "$produk->judul | $produk->penerbit | $produk->penulis | Rp.$produk->harga";
+        return $str;
+    }
+}
+
+$produk1 = new Produk("Naruto", "Masashi Nishimoto", "Erlangga", 100000);
+$produk2 = new Produk("Uncharted", "Faisal", "SIDU", 200000);
+$produk3 = new Produk("Doraemon", "Nishimoto", "Erlangga", 500000);
+$produk4 = new Produk();
+
+//var_dump($produk1);
+//var_dump($produk2);
+
+echo $produk1 -> getLabel();
+echo "<br>";
+echo $produk2 -> getLabel();
+echo "<br>";
+echo $produk3 -> getLabel();
+echo "<br>";
+echo $produk4 -> getLabel();
+echo "<br>";
+
+$infoproduk = new CetakInfoProduk();
+echo $infoproduk->cetak($produk2);
+//echo
+
+
+?>
