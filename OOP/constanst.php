fd4b2c77ba111e2892be591b8bb34ec01bb5e21e
andrekelas4A <yuliushermawan813@gmail.com>
2023-04-09 15:42:29 -0700

    tambah file constanst

---
+++ OOP/constanst.php
@@ -0,0 +1,16 @@
+<?php 
+// define('NAMA', 'Andre');
+// echo NAMA;
+
+//echo "<br>";
+
+//const NIP = 1234;
+//echo NIP;
+
+class Coba {
+    //define('NAMA, 'Andre');
+    const NAMA = 'Yulius Andre Dwi Hermawan';
+}
+
+echo Coba::NAMA;
+?>
